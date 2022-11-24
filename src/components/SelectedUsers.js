b4590a71d08cdd729d6b7b9c50cf0f4d86f7f34f
Pakzad <pakzadlive@gmail.com>
2022-11-24 14:32:14 +0330

    adding remove button to selected users

--- src/components/SelectedUsers.js
+++ src/components/SelectedUsers.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { CartContext } from "../context/CartContextProvider";
+import { isInCart, quantityCount } from "../helper/functions";
 
 import {
   Box,
@@ -14,21 +17,68 @@ import {
 const theme = createTheme();
 
 const SelectedUsers = ({ selectedUser }) => {
+  const { state, dispatch } = useContext(CartContext);
+
   return (
     <ThemeProvider theme={theme}>
       <Box>
         <Container sx={{ padding: 2 }} maxWidth="md">
           <Card sx={{ padding: 5 }}>
-            <Typography component="h5" variant="h7">
+            <Typography
+              component="h5"
+              variant="h7"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               Name
             </Typography>
-            <Typography gutterBottom component="h2">
+            <Typography
+              gutterBottom
+              component="h2"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               {selectedUser.name}
             </Typography>
-            <Typography component="h5" variant="h7">
+            <Typography
+              component="h5"
+              variant="h7"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               Email
             </Typography>
-            <Typography>{selectedUser.email}</Typography>
+            <Typography
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {selectedUser.email}
+            </Typography>
+
+            <Grid item sx={{ display: "flex", justifyContent: "center" }}>
+              {quantityCount(state, selectedUser.id) === 1 && (
+                <Button
+                  size="small"
+                  onClick={() =>
+                    dispatch({ type: "REMOVE_ITEM", payload: selectedUser })
+                  }
+                >
+                  Remove
+                </Button>
+              )}
+            </Grid>
           </Card>
         </Container>
       </Box>
